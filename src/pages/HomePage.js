8776192b86c777d71781271d703d15f452ed2d0b
hongweihsu <hongweih@student.unimelb.edu.au>
2022-02-08 15:59:51 +0800

    fix uuid key issue and available to generate a new dogs set

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,57 +1,52 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import Search from "../components/Search";
-import Picture from '../components/Picture';
-import {v4 as uuidv4} from "uuid";
+import Picture from "../components/Picture";
+import { v4 as uuidv4 } from "uuid";
 
 const HomePage = () => {
   let condition = 8;
-  let supportFile = ['jpg', 'jpeg', 'png', 'gif']
+  let temp = [];
+  let supportFile = ["jpg", "jpeg", "png", "gif"];
   let [data, setData] = useState([]);
-  const apiURL = "https://random.dog/woof.json" 
-  
-  const search = async() => {
-      // if (data.length >= 8){
-      //     data = [];
-      //     console.log("reset");
-      // }
-      setData([]);
+  let [currentData, setCurrentData] = useState([]);
+  const apiURL = "https://random.dog/woof.json";
 
-      while (data.length < condition) {
-        const dataFetch = await fetch(apiURL, {method: "GET", headers: {Accept:"application/json"}});
-        let parsedData = await dataFetch.json();
-        let url = parsedData.url;
-        
-        supportFile.forEach(element => {
-          if (url.toString().includes(element)){
-            console.log(url);
-            data.push({url, id: uuidv4()});
-          }
-        });
-      } 
-      setData(data);
-      console.log("dataArr")
-      console.log(data);
+  const search = async () => {
+    // setData([]);
+    while (temp.length < condition) {
+      const dataFetch = await fetch(apiURL, {
+        method: "GET",
+        headers: { Accept: "application/json" },
+      });
+      let parsedData = await dataFetch.json();
+      let url = parsedData.url;
+
+      supportFile.forEach((element) => {
+        if (url.toString().includes(element)) {
+          console.log(url);
+          temp.push({ url, id: uuidv4() });
+        }
+      });
+    }
+    setData(temp);
+    console.log("dataArr");
+    console.log(temp);
   };
-  
+
   useEffect(() => {
     search();
   }, []);
 
-  // useEffect(() => {
-  //   search();
-  // }, [data]);
-
   return (
     <div style={{ minHeight: "100vh" }}>
       <h1>Welcome Happy Dogs Land</h1>
-      <Search search = {search} />
-      <div className='pictures'>
-        { data&&
+      <Search search={search} />
+      <div className="pictures">
+        {data &&
           data.map((d) => {
-            console.log("dataMap")
-            return <Picture url={d.url} />;
-          })
-        }
+            console.log("dataMap");
+            return <Picture url={d.url} key={d.id} />;
+          })}
       </div>
     </div>
   );
